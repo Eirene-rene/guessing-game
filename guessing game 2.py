3314ef641187a8c72776b7680b92524a7a600ec1
eirene-rene <rebekahsofo@gmail.com>
2022-04-11 20:39:34 +0000

    Built guessing game

---
+++ guessing game 2.py
@@ -0,0 +1,7 @@
+number = int( input( "guess a number :"))
+if number == 10:
+    print (" hooray i got it")
+elif number >10:
+    print ("try again next time ")
+elif number <10:
+    print ("try again next time")
